134cb19da8c298d26dda0ec47bffed49815d7979
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-13 10:21:47 +0000

    Set route = [None] * length - 1 to remove extra None value

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,7 +14,7 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
+    route = [None] * (length - 1)
 
     """
     YOUR CODE HERE
